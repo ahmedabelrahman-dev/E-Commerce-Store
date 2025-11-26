8cfc9e153ec0265c7d9885c1e35606ee5bf5631e
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-26 22:42:45 +0400

    feat: added the route for catagory pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import AdminPage from './pages/AdminPage';
+import CategoryPage from './pages/CategoryPage';
 
 import Navbar from './components/Navbar';
 import { Toaster } from 'react-hot-toast';
@@ -44,6 +45,7 @@ function App() {
               user?.role === 'admin' ? <AdminPage /> : <Navigate to="/login" />
             }
           />
+          <Route path="/category/:category" element={<CategoryPage />} />
         </Routes>
       </div>
       <Toaster />
