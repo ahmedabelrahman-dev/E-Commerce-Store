dc524369dfa425487a69d75c4448f541cccaa4e7
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-26 21:41:25 +0400

    feat: added the route for admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
+import AdminPage from './pages/AdminPage';
 
 import Navbar from './components/Navbar';
 import { Toaster } from 'react-hot-toast';
